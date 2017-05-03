ab7267f4ddae149fa4165bc8bdafa87e8d941981
albert.denova <albert.denova@king.com>
2017-05-03 21:53:18 +0200

    Push everything

---
+++ TicTacToe/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Board {
+	public enum CurrentPlayer {
+		PLAYER_CIRCLE,
+		PLAYER_CROSS,
+		PLAYER_INVALID
+	}
+}
+
+public class BoardManager : MonoBehaviour {
+
+	public GameObject GroundObject;
+	public int BoardSize = 3;
+
+	public float UnitsPerGroundObject = 3f;
+	public float MarginBetweenGroundTiles = 0.2f;
+
+	private Board.CurrentPlayer _currentTurn = Board.CurrentPlayer.PLAYER_CIRCLE;
+
+	private struct BoardPosition {
+		public Board.CurrentPlayer currentPlayer;
+		public long gameObjectInstanceId;
+
+		public BoardPosition(long instanceId) 
+		{
+			currentPlayer = Board.CurrentPlayer.PLAYER_INVALID;
+			gameObjectInstanceId = instanceId;
+		}
+
+	};
+
+	private List<List<BoardPosition>> _board;
+
+	private Transform _boardHolder;
+	
+	// Use this for initialization
+	void Start () {
+		CreateBoard();
+		NotifyCurrentTurn();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnItemSelected(int instanceId) 
+	{
+		_currentTurn = (int) Board.CurrentPlayer.PLAYER_CROSS - _currentTurn;
+		NotifyCurrentTurn();
+	}
+
+	private void CreateBoard() 
+	{
+		_boardHolder = new GameObject("BoardHolder").transform;
+		_boardHolder.SetParent(transform);
+
+		var leftOffset = 0f;
+		var topOffset = 0f;
+
+		_board = new List<List<BoardPosition>>();
+		for(var i = 0; i < BoardSize; ++i)
+		{
+			leftOffset = 0f;
+			List<BoardPosition> rowItems = new List<BoardPosition>();
+			for(var j = 0; j < BoardSize; ++j)
+			{
+				var groundTile = Instantiate(GroundObject, 
+										 new Vector3(leftOffset, topOffset, 0f), 
+										 Quaternion.identity, 
+										 _boardHolder);				
+				leftOffset += UnitsPerGroundObject + MarginBetweenGroundTiles;			
+				BoardPosition boardPosition = new BoardPosition(groundTile.GetInstanceID());
+				rowItems.Add(boardPosition);
+			}
+
+			topOffset -= UnitsPerGroundObject + MarginBetweenGroundTiles;
+
+			_board.Add(rowItems);
+			
+		}
+
+		_boardHolder.position = new Vector2(-(UnitsPerGroundObject + MarginBetweenGroundTiles), 
+											(UnitsPerGroundObject + MarginBetweenGroundTiles));
+	}
+
+	private void NotifyCurrentTurn() 
+	{
+		_boardHolder.BroadcastMessage("SetCurrentPlayerTurn", _currentTurn);
+	}
+}
---
+++ TicTacToe/Assets/Scripts/GroundTileComponent.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTileComponent : MonoBehaviour {
+
+	public Sprite GroundSprite;
+	public Sprite CrossSprite;
+	public Sprite CircleSprite;
+	private bool _isDisplayingOver = false;
+
+	private Board.CurrentPlayer _currentPlayerTurn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		var isOver = IsInsideObject(mousePosition);
+		DisplayActiveTileBackground(isOver);
+		
+		if(isOver && Input.GetMouseButtonDown(0))
+		{
+			//BoardManager boardManager = GameObject.Find("BoardManager").GetComponent<BoardManager>();
+			BoardManager boardManager = GetComponentInParent<BoardManager>();
+			boardManager.OnItemSelected(GetInstanceID());
+		}
+	}
+
+	public void SetCurrentPlayerTurn(Board.CurrentPlayer currentPlayerTurn) 
+	{
+		_currentPlayerTurn = currentPlayerTurn;
+	}
+
+	private bool IsInsideObject(Vector3 mousePosition) 
+	{
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+		Vector3 minBounds = spriteRenderer.bounds.min;
+		Vector3 maxBounds = spriteRenderer.bounds.max;
+
+		return IsBetween(minBounds.x, maxBounds.x, mousePosition.x) &&
+		       IsBetween(minBounds.y, maxBounds.y, mousePosition.y);
+	}
+
+	private bool IsBetween(float firstNumber, float secondNumber, float value)
+	{
+		return (value >= firstNumber) && (value <= secondNumber);		
+	} 
+
+	private void DisplayActiveTileBackground(bool isMouseOver) 
+	{		
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+		if(isMouseOver && !_isDisplayingOver)
+		{
+			_isDisplayingOver = true;
+			if(_currentPlayerTurn == Board.CurrentPlayer.PLAYER_CIRCLE)
+			{
+				spriteRenderer.sprite = CircleSprite;
+			}
+			else 
+			{
+				spriteRenderer.sprite = CrossSprite;
+			}
+			
+		}
+		else if(!isMouseOver && _isDisplayingOver)
+		{
+			_isDisplayingOver = false;
+			spriteRenderer.sprite = GroundSprite;
+		}
+	}
+}
